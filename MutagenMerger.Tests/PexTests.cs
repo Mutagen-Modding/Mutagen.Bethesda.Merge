08cea7ed99804ec3844639d60a4b6cea1cc28977
erri120 <erri120@protonmail.com>
2021-03-23 16:26:53 +0100

    Update unit test

--- MutagenMerger.Tests/PexTests.cs
+++ MutagenMerger.Tests/PexTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using MutagenMerger.Pex;
+using MutagenMerger.Pex.Extensions;
 using Xunit;
 
 namespace MutagenMerger.Tests
@@ -34,6 +36,7 @@ namespace MutagenMerger.Tests
             Assert.True(File.Exists(path));
 
             var pex = PexParser.ParsePexFile(path);
+            Assert.NotNull(pex);
         }
 
         [Theory]
@@ -50,11 +53,61 @@ namespace MutagenMerger.Tests
             Assert.True(File.Exists(outputFile));
 
             var outputPex = PexParser.ParsePexFile(outputFile);
+            Assert.NotNull(outputPex);
             
             var inputFi = new FileInfo(inputFile);
             var outputFi = new FileInfo(outputFile);
             
             Assert.Equal(inputFi.Length, outputFi.Length);
+        }
+
+        [Fact]
+        public void TestSinglePexParsing()
+        {
+            var path = Path.Combine("files", "Art.pex");
+            Assert.True(File.Exists(path));
+
+            var pex = PexParser.ParsePexFile(path);
+            
+            Assert.Equal(0xFA57C0DE, pex.Magic);
+            Assert.Equal(3, pex.MajorVersion);
+            Assert.Equal(2, pex.MinorVersion);
+            Assert.Equal(1, pex.GameId);
+            Assert.Equal(((ulong) 0x5F21B0ED).ToDateTime(), pex.CompilationTime);
+            Assert.Equal("Art.psc", pex.SourceFileName);
+            Assert.Equal(string.Empty, pex.Username);
+            Assert.Equal(string.Empty, pex.MachineName);
+
+            var stringTable = pex.StringTable;
+            var debugInfo = pex.DebugInfo;
+            var userFlagsTable = pex.UserFlags;
+            
+            Assert.NotNull(stringTable);
+            Assert.NotNull(debugInfo);
+            Assert.NotNull(userFlagsTable);
+
+            {
+                Assert.True(debugInfo.HasDebugInfo);
+                Assert.Equal(4, debugInfo.Functions.Count);
+            }
+
+            var objects = pex.Objects;
+            Assert.Single(objects);
+
+            var mainObject = objects.First();
+            
+            Assert.Equal("Art", mainObject.GetName(stringTable));
+            Assert.Equal("Form", mainObject.GetParentClassName(stringTable));
+            Assert.Equal(string.Empty, mainObject.GetDocString(stringTable));
+            Assert.Equal(string.Empty, mainObject.GetAutoStateName(stringTable));
+            
+            Assert.Empty(mainObject.Properties);
+            Assert.Empty(mainObject.Variables);
+            Assert.Single(mainObject.States);
+
+            var state = mainObject.States.First();
+            Assert.Equal(string.Empty, state.GetName(stringTable));
+            Assert.Equal(4, state.Functions.Count);
         }
     }
 }
